9a6e56fc8affa59e465d14386c50a8d7516cfc26
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-08-28 19:18:23 -0400

    cambio en el despliegue, para que sea estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  output: 'static',
+  adapter: cloudflare(),
+  site: 'https://sanhueza.pro',
 });
